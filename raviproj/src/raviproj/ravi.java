f7a4b4f63c7d85a5ebf3b0b62669f24eb6553497
ravi <rk@gmail.com>
2018-08-28 16:22:36 +0530

    delete unnecessary

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -10,16 +10,7 @@ public class ravi {
 		
 		//dev1
 
-		System.out.println("rdev1 rdev2 modifying  my nice first msg");
-		//dev1
-		System.out.println("rdev2   my nice first msg");
-
-
-		System.out.println("git tutorial 2");
-
-		System.out.println("rdev1 ravi my nice first msg");
-		System.out.println("git tutorial 1");
-
+		
 	}
 
 }
