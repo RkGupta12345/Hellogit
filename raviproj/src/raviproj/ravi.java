8b8e6b08acf788e39c43b54da7b5b8047fff31d3
ravi <rk@gmail.com>
2018-08-28 16:10:04 +0530

    tttt

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -10,7 +10,7 @@ public class ravi {
 		
 		//dev1
 
-		System.out.println("rdev1 rdev2 modifying  my nice first msg");
+		System.out.println("rdev1 rdev2 modifyin.g  my nice first msg");
 		//dev1
 		System.out.println("rdev2   my nice first msg");
 
