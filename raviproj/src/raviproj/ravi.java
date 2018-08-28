d373ac92e5ab7501f2ec3df46576744988954fd4
ravi <rk@gmail.com>
2018-08-28 12:13:25 +0530

    dev2 first commit

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -4,6 +4,9 @@ public class ravi {
 
 	public static void main(String[] args) {
 		System.out.println("rdev1  my nice first msg");
+		
+		//dev2
+		System.out.println("rdev2  my nice first msg");
 
 	}
 
