1c61f08f60288e4469fc98c4bc969e5f9af536c6
ravi <rk@gmail.com>
2018-08-28 12:19:03 +0530

    dev2 2nd commit

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -3,7 +3,7 @@ package raviproj;
 public class ravi {
 
 	public static void main(String[] args) {
-		System.out.println("rdev1  my nice first msg");
+		System.out.println("rdev2 modifying  my nice first msg");
 		
 		//dev1
 		System.out.println("rdev1 modifying  my nice first msg");
