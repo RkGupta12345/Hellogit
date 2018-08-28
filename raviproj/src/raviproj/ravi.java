777947358952fc37d47e21475ac175d5d99a7890
ravi <rk@gmail.com>
2018-08-28 12:16:14 +0530

    dev1 2n d commit

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -5,8 +5,8 @@ public class ravi {
 	public static void main(String[] args) {
 		System.out.println("rdev1  my nice first msg");
 		
-		//dev2
-		System.out.println("rdev2  my nice first msg");
+		//dev1
+		System.out.println("rdev1 modifying  my nice first msg");
 
 	}
 
