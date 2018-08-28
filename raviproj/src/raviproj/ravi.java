42bf58a918ae5ef184ff046909db9703fb77fe4d
munnaf <munaf.abdul9071@gmail.com>
2018-08-28 15:42:22 +0530

    munaf commit first

--- raviproj/src/raviproj/ravi.java
+++ raviproj/src/raviproj/ravi.java
@@ -3,23 +3,8 @@ package raviproj;
 public class ravi {
 
 	public static void main(String[] args) {
-		   int k=10;
-		     int l=10;
-				int total =k+l*k;
-		System.out.println("sum"+total);
-		
-		//dev1
-
-		System.out.println("rdev1 rdev2 modifying  my nice first msg");
-		//dev1
-		System.out.println("rdev2   my nice first msg");
-
-
-		System.out.println("git tutorial 2");
-
-		System.out.println("rdev1 ravi my nice first msg");
-		System.out.println("git tutorial 1");
-
+		  int a=10;
+		  System.out.println(" MUNnaf --->variable "+a);
 	}
 
 }
